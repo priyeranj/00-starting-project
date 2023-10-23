68734a0856b4ee342ee3729e46faa2c536015f30
Priyaranjan Kumar <priyaranjan.moscow@gmail.com>
2023-10-23 20:31:24 +0530

    converted html files under views folder to EJS

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const express = require("express");
 
 const app = express();
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
 
 //Middleware
 app.use(express.static("public"));
